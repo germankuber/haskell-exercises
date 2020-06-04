62c0542c19b56e2f2f5a93153985dc2d00743d0a
Germán Küber <german.kuber@outlook.com>
2020-06-04 10:13:36 -0300

    Added 16

--- 16.hs
+++ 16.hs
@@ -5,13 +5,13 @@ groupById list = groupBy (\x y -> fst x == fst y) list
 sortGT (a1, b1) (a2, b2)
   | b1 < b2 = GT
   | b1 > b2 = LT
-  | b1 == b2 = compare b1 b2
+  | b1 == b2 = EQ
 
-getAvergares notes = div (sum (map (\(_, note) -> note) notes)) (length notes)
+getAvergares notes = div (sum.map snd $ notes) (length notes)
 
 
 
 writeString list = map (\(a,b)-> ("Legajo : " ++ show a, "Promedio : " ++ show b)) list
 
 
-giveMeResult list =   writeString (take 2 (sortBy sortGT (foldl (\acc x -> (fst $ head x , getAvergares x) : acc  ) [] $ groupById list)))
+giveMeResult list =   writeString (take 2 (sortBy sortGT (map (\ x -> (fst $ head x , getAvergares x) ) $ groupById list)))
